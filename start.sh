9484cb42c8a5f468c914645fcc71783bd70bf8fd
Deep Patel <deep.researchnode@gmail.com>
2021-10-02 15:51:51 +0530

    gazebo on start

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -i
 git pull
+rosrun xacro xacro --inorder -o /catkin_ws/src/youbot_description/robots/youbot.urdf /catkin_ws/src/youbot_description/robots/youbot.urdf.xacro
 
 echo "============Please open new tab and start docker container there==========="
 catkin build
 source devel/setup.bash
+roslaunch gazebo_ros empty_world.launch
 rosrun core spin.py
 
 
